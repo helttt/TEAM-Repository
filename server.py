7ebfe173df4a5800d095559582ef84e05d363e5a
yellow-akihiro <403143346@qq.com>
2020-11-07 19:59:19 +0800

    加入学生数据

--- server.py
+++ server.py
@@ -12,13 +12,22 @@ def home():
 @app.route('/result', methods=['GET'])
 def result():
     sql = sqlite3.connect("data.db")
-    string = ""
+    data = {}
     for i in range(1, 12):
-        data = len(list(sql.execute('select * from vote where gid=?', (i,))))
-        string += '第' + str(i) + '组: ' + str(data) + '\r\n'
+        data[i] = len(
+            list(sql.execute('select * from vote where gid=?', (i,))))
     sql.close()
-    return string
+    return render_template("result.html", data=data)
 
+@app.route('/data', methods=['POST'])
+def data():
+    sql = sqlite3.connect("data.db")
+    string = ""
+    data2 = list(sql.execute('select * from vote order by sid'))
+    for i in data2:
+        string += i[0] + ' 投给第' + str(i[1]) + '组\r\n'
+    sql.close()
+    return render_template('data.html', string=string)
 
 @app.route('/index', methods=['POST'])
 def index():
